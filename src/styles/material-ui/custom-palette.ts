71bbfa7a385a0a61ef8afa6bdbd4109c625b5e0c
iwamoto65 <kosuke00940@icloud.com>
2021-09-04 18:46:38 +0900

    material-uiのデフォルトテーマを変更した

---
+++ src/styles/material-ui/custom-palette.ts
@@ -0,0 +1,8 @@
+export const customizedPalette = {
+  primary: {
+    main: '#90caf9'
+  },
+  secondary: {
+    main: '#f48fb1'
+  }
+}
---
+++ src/styles/material-ui/custom-style.ts
@@ -0,0 +1,6 @@
+import createTheme from '@material-ui/core/styles/createTheme';
+import { customizedPalette } from "./custom-palette";
+
+export const theme = createTheme({
+  palette: customizedPalette
+})
